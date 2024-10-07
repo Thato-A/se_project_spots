25c50820b11121a95e18df0de5d55255580d6cf1
Lily Anderson <thatoanderson92@gmail.com>
2024-10-07 15:26:51 -0400

    fix close buttons in JS

--- script/index.js
+++ script/index.js
@@ -36,7 +36,8 @@ const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
 const editFormElement = editModal.querySelector(".modal__form");
-const editModalCloseBtn = document.querySelector(".modal__close-btn");
+const closeButtons = [...document.querySelectorAll(".modal__close-btn")];
+
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
@@ -44,16 +45,16 @@ const editModalDescriptionInput = editModal.querySelector(
 
 const cardModal = document.querySelector("#add-card-modal");
 const cardForm = cardModal.querySelector(".modal__form");
-const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
+//const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
-const previewModalCloseBtn = previewModal.querySelector(
-  ".modal__close_type_preview"
-);
+//const previewModalCloseBtn = previewModal.querySelector(
+// ".modal__close-btn_type_preview"
+//);
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
@@ -113,6 +114,7 @@ function handleAddCardSubmit(evt) {
   closeModal(cardModal);
   const cardElement = getCardElement(inputValues);
   cardsList.prepend(cardElement);
+  evt.target.reset();
 }
 
 profileEditButton.addEventListener("click", () => {
@@ -121,25 +123,26 @@ profileEditButton.addEventListener("click", () => {
   openModal(editModal);
 });
 
-editModalCloseBtn.addEventListener("click", () => {
-  closeModal(editModal);
+closeButtons.forEach((button) => {
+  const popup = button.closest(".modal");
+  button.addEventListener("click", () => closeModal(popup));
 });
 
 cardModalBtn.addEventListener("click", () => {
   openModal(cardModal);
 });
 
-cardModalCloseBtn.addEventListener("click", () => {
-  closeModal(cardModal);
-});
+//cardModalCloseBtn.addEventListener("click", () => {
+// closeModal(cardModal);
+//});
 
 editFormElement.addEventListener("submit", handleEditFormSubmit);
 
 cardForm.addEventListener("submit", handleAddCardSubmit);
 
-previewModalCloseBtn.addEventListener("click", () => {
-  closeModal(previewModal);
-});
+//previewModalCloseBtn.addEventListener("click", () => {
+// closeModal(previewModal);
+//});
 
 initialCards.forEach((item) => {
   const cardElement = getCardElement(item);
